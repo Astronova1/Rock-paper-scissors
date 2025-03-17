8d503365e8839a8e9ab6e58e99000f9f77fc8577
Awaiz <awaiz123nasir123@gmail.com>
2025-03-17 22:32:32 +0500

    add function to play the current round

--- javascript.js
+++ javascript.js
@@ -17,3 +17,50 @@ function getHumanChoice(){
     return userChoice
 }
 
+let humanScore = 0 , computerScore = 0;
+
+function playRound(humanSelection,computerSelection){
+   let humanChoice = humanSelection.toLowerCase();
+   if (computerSelection=== 'rock'){
+        if (humanChoice === 'paper'){
+            console.log('Paper beast Rock! You Win');
+            humanScore++;
+        }
+        else if (humanChoice === 'scisscors'){
+            console.log('rock beast scissors. You lost!');
+            computerScore++;
+        }
+        else{
+            console.log('It\'s a tie');
+        }
+   }
+   else if (computerSelection === 'paper'){
+    if (humanChoice === 'rock'){
+        console.log('paper beats rock. You Lost!');
+        computerScore++;
+    }
+    else if (humanChoice === 'scissors'){
+        console.log('scissors beats paper. You won!');
+        humanScore++;
+    }
+    else {
+        console.log("It's a tie");
+    }
+   }
+   else if (computerSelection === 'scissors'){
+    if (humanChoice === 'rock'){
+        console.log('rock beats scissors. You Won!')
+        humanScore++;
+    }
+    if (humanChoice === 'paper'){
+        console.log('scissors beats paper. You lost!')
+        computerScore++;
+    }
+    else {
+        console.log('It\'s a tie')
+    }
+   }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
