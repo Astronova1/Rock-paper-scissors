73637778127272c87eac2a714a6cdbede3b5ea90
Awaiz <awaiz123nasir123@gmail.com>
2025-03-30 17:43:52 +0500

    fix showing message in console when loading the page

--- javascript.js
+++ javascript.js
@@ -21,72 +21,89 @@ let humanSelection;
 let computerSelection;
 
 
-    let humanScore = 0 , computerScore = 0;
+    let humanScore = 0 , computerScore = 0, round = 0;
     function playRound(humanSelection){
         computerSelection = getComputerChoice();
         let humanChoice = humanSelection.toLowerCase();
         if (computerSelection=== 'rock'){
              if (humanChoice === 'paper'){
-                 console.log('Paper beast Rock! You Win');
+                 div.textContent = 'Paper beast Rock! You Win';
                  humanScore++;
+                 round++;
              }
              else if (humanChoice === 'scissors'){
-                 console.log('rock beast scissors. You lost!');
+                 div.textContent = 'rock beast scissors. You lost!';
                  computerScore++;
+                 round++;
              }
              else{
-                 console.log('It\'s a tie');
+                 div.textContent = 'It\'s a tie';
+                 round++;
              }
         }
         else if (computerSelection === 'paper'){
          if (humanChoice === 'rock'){
-             console.log('paper beats rock. You Lost!');
+             div.textContent = 'paper beats rock. You Lost!';
              computerScore++;
+             round++;
          }
          else if (humanChoice === 'scissors'){
-             console.log('scissors beats paper. You won!');
+             div.textContent = 'scissors beats paper. You won!';
              humanScore++;
+             round++;
          }
          else {
-             console.log("It's a tie");
+             div.textContent = "It's a tie";
+             round++;
          }
         }
         else if (computerSelection === 'scissors'){
          if (humanChoice === 'rock'){
-             console.log('rock beats scissors. You Won!')
+             div.textContent = 'rock beats scissors. You Won!';
              humanScore++;
+             round++;
          }
          else if (humanChoice === 'paper'){
-             console.log('scissors beats paper. You lost!')
+             div.textContent = 'scissors beats paper. You lost!';
              computerScore++;
+             round++;
          }
          else {
-             console.log('It\'s a tie')
+             div.textContent = 'It\'s a tie';
+             round++;
             }
         }
     }
 
-    if (humanScore > computerScore){
-        console.log('You won the Game!')
+    if (round){
+        if (humanScore > computerScore){
+            console.log('You won the Game!')
+        }
+        else if (computerScore > humanScore){
+            console.log('The Computer Won the Game')
+        }
+        else {
+            console.log('Nobody won this Game')
+        }
     }
-    else if (computerScore > humanScore){
-        console.log('The Computer Won the Game')
-    }
-    else {
-        console.log('Nobody won this Game')
-    }
+    
+        const rbtn = document.querySelector("#rock");
+        rbtn.addEventListener("click", () => {
+            playRound("rock");
+        });
 
-    const rbtn = document.querySelector("#rock");
-    rbtn.addEventListener("click", () => {
-        playRound("rock");
-    });
+        const pbtn = document.querySelector("#paper");
+        pbtn.addEventListener("click", () => {
+            playRound("paper"); 
+        });
 
-     const pbtn = document.querySelector("#paper");
-     pbtn.addEventListener("click", () => {
-        playRound("paper"); 
-     });
+        const sbtn = document.querySelector("#scissors");
+        sbtn.addEventListener("click", () => {
+            playRound("scissors");
+        });
 
-     const sbtn = document.querySelector("#scissors");
-     sbtn.addEventListener("click", () => {
-        playRound("scissors");
-     });
+
+
+const div = document.createElement("div");
+div.classList.add("result");
+document.body.appendChild(div);
